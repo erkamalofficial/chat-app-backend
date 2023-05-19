48a2f2f1c6da7fb15d8e8084cad5e575296e4248
dksingla <deepak.stoic@gmail.com>
2023-05-19 09:26:49 +0530

    Backend updated

--- src/index.js
+++ src/index.js
@@ -9,24 +9,6 @@ app.use(cors());
 
 
 
-// -----------------------------------------------------------------------------
-
-app.get('/nature/:text', (req, res) => {
-    const { text } = req.params
-    try {
-        if (!text) return res.status(400).json("Bad Request");
-        const tokenizer = new natural.WordTokenizer();
-        const textTokens = tokenizer.tokenize(text)
-        const analyzer = new natural.SentimentAnalyzer("English", natural.PorterStemmer, "afinn");
-        const comparative = analyzer.getSentiment(textTokens)
-        const expression = comparative > 0 ? "positive" : comparative < 0 ? 'negitive' : 'neutral'
-        res.status(200).json(expression);
-    } catch (error) {
-        res.status(500).json(error)
-    }
-});
-
-// -----------------------------------------------------------------------------
 
 const server = app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
